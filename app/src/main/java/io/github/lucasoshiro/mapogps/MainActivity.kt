edf207b51a8a05aea929a9fdb920a6045c136861
Lucas Oshiro <lucasseikioshiro@gmail.com>
2022-04-16 22:52:20 -0300

    Initial commit

---
+++ app/src/main/java/io/github/lucasoshiro/mapogps/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.lucasoshiro.mapogps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
