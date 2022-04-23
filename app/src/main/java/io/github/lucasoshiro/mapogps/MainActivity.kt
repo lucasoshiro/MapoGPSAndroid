a774e150ecbdd8d04494f6649e50880aed9fbe4a
Lucas Oshiro <lucasseikioshiro@gmail.com>
2022-04-23 14:10:35 -0300

    update GPS coordinates using Locus

--- app/src/main/java/io/github/lucasoshiro/mapogps/MainActivity.kt
+++ app/src/main/java/io/github/lucasoshiro/mapogps/MainActivity.kt
@@ -2,7 +2,9 @@ package io.github.lucasoshiro.mapogps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.birjuvachhani.locus.Locus
 import io.github.lucasoshiro.mapogps.core.Coord
 import io.github.lucasoshiro.mapogps.core.mapoFromCoord
 
@@ -10,21 +12,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Locus.startLocationUpdates(this) { result ->
+            result.location?.let {
+                this.updateWidgets(it.latitude to it.longitude)
+            }
+        }
     }
 
-    private fun updateWidgets() {
+    private fun updateWidgets(coord: Coord) {
         val pageTextView: TextView = findViewById(R.id.page)
         val pageCoordinatesTextView: TextView = findViewById(R.id.pageCoordinates)
 
+        val (page, letter, number) = mapoFromCoord(coord.first, coord.second)
+
+        Log.println(Log.INFO, "mapoGPS", "$coord")
+
         pageTextView.text = page
         pageCoordinatesTextView.text = "$letter $number"
-    }
-
-    companion object {
-        var coordinates: Coord = -23.5 to -46.6
-        var mapoCoordinates = mapoFromCoord(coordinates.first, coordinates.second)
-        var page = mapoCoordinates.first
-        var letter = mapoCoordinates.second
-        var number = mapoCoordinates.third
     }
 }
