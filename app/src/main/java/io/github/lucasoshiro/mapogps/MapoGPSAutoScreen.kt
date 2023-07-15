88f2026be92dd28bc3cad6583f6fe5bccb918db2
Lucas Oshiro <lucasseikioshiro@gmail.com>
2023-07-15 19:05:45 -0300

    update auto screen

--- app/src/main/java/io/github/lucasoshiro/mapogps/MapoGPSAutoScreen.kt
+++ app/src/main/java/io/github/lucasoshiro/mapogps/MapoGPSAutoScreen.kt
@@ -3,13 +3,22 @@ package io.github.lucasoshiro.mapogps
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
 import androidx.car.app.model.*
+import io.github.lucasoshiro.mapogps.core.Coord
+import io.github.lucasoshiro.mapogps.core.mapoFromCoord
 
 class MapoGPSAutoScreen(
-    carContext: CarContext
+    carContext: CarContext,
+    private val coord: Coord?
 ) : Screen(carContext) {
     override fun onGetTemplate(): Template {
-        val row = Row.Builder().setTitle("Hello world!").build()
+        val displayStr = coord?.let {
+            val (page, letter, number) = mapoFromCoord(it.first, it.second)
+            return@let "$page\n$letter $number"
+        } ?: ""
+
+        val row = Row.Builder().setTitle("  ").addText(displayStr).build()
         val pane = Pane.Builder().addRow(row).build()
+
         return PaneTemplate.Builder(pane)
             .setHeaderAction(Action.APP_ICON)
             .build()
--- app/src/main/java/io/github/lucasoshiro/mapogps/MapoGPSAutoSession.kt
+++ app/src/main/java/io/github/lucasoshiro/mapogps/MapoGPSAutoSession.kt
@@ -1,12 +1,32 @@
 package io.github.lucasoshiro.mapogps
 
+import android.content.Context
 import android.content.Intent
+import android.util.Log
+import androidx.car.app.CarContext
 import androidx.car.app.Screen
+import androidx.car.app.ScreenManager
 import androidx.car.app.Session
+import androidx.lifecycle.LifecycleOwner
+import com.birjuvachhani.locus.Locus
 
 class MapoGPSAutoSession : Session() {
     override fun onCreateScreen(intent: Intent): Screen {
-        return MapoGPSAutoScreen(carContext)
-    }
+        val screen = MapoGPSAutoScreen(carContext, null)
+        val liveData = Locus.startLocationUpdates(carContext)
 
+        liveData.observeForever { result ->
+            result.location?.let {
+                val screenManager = carContext.getCarService(CarContext.SCREEN_SERVICE) as ScreenManager
+                if (screenManager.stackSize > 0) {
+                    screenManager.top.invalidate()
+
+                    screenManager.push(
+                        MapoGPSAutoScreen(carContext, it.latitude to it.longitude)
+                    )
+                }
+            }
+        }
+        return screen
+    }
 }
